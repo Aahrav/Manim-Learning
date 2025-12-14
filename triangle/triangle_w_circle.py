a39e31ded7b219d4a3bdcea94f1e635d2a880e50
Aahrav <aaravparmar2408@gmail.com>
2025-12-14 12:50:28 +0000

    started triangle from three circles,basic setup and debugging

---
+++ triangle/triangle_w_circle.py
@@ -0,0 +1,108 @@
+from manim import *
+import numpy as np
+
+class TriangleScene(Scene):
+    def construct(self):
+        # create circles
+        circle1 = Circle(radius=3, color=RED).shift(LEFT * 2).shift(DOWN * 0.8)
+        circle2 = Circle(radius=3, color=BLUE).shift(RIGHT * 2).shift(DOWN * 0.8)
+        circle3 = Circle(radius=3, color=YELLOW)
+
+        midpoint = np.array([0, -0.8, 0])
+        h = 2 * np.sqrt(3)
+        circle3.move_to(midpoint + np.array([0, h, 0]))
+
+        # store in lists
+        circles = [circle1, circle2, circle3]
+        centers = [c.get_center() for c in circles]
+        radii = [c.radius for c in circles]
+
+        # signed area test, if sign negative we reverse the order
+        area = 0
+        for i in range(3):
+            x1, y1 = centers[i][0], centers[i][1]
+            x2, y2 = centers[(i + 1) % 3][0], centers[(i + 1) % 3][1]
+            area += x1 * y2 - x2 * y1
+
+        # force CCW order
+        if area < 0:
+            centers.reverse()
+            circles.reverse()
+            radii.reverse()
+
+        # debug labels
+        labels = VGroup()
+        for i, c in enumerate(centers):
+            labels.add(Text(str(i+1), font_size=24).next_to(c, UP))
+
+        area_text = Text(f"{area:.2f}", font_size=28).to_corner(UL)
+        g = 0
+        # calculating centroid of the triangle
+        for i in range(3):
+            g += centers[i]
+        g /= 3
+        # outward_dirs from interior -> centers ci -g
+        outward_dirs = [centers[0] - g,centers[1] - g,centers[2] - g]
+        outward_dirs = [v / np.linalg.norm(v) for v in outward_dirs]
+
+        
+   
+
+        def circle_circle_intersections(c1, r1, c2, r2, eps=1e-8):
+            
+    
+            v = c2 - c1
+            d = np.linalg.norm(v)
+    
+            # Same center → infinite or none (ignore)
+            if d < eps:
+                return []
+    
+            # Too far apart or one inside the other → no intersection
+            if d > r1 + r2 + eps or d < abs(r1 - r2) - eps:
+                return []
+    
+            # Distance from c1 to midpoint between intersections
+            a = (r1**2 - r2**2 + d**2) / (2 * d)
+    
+            h_sq = r1**2 - a**2
+            if abs(h_sq) < eps:
+                h_sq = 0.0
+            if h_sq < 0:
+                return []
+    
+            h = np.sqrt(h_sq)
+    
+            # Base point
+            p0 = c1 + a * v / d
+    
+            # Perpendicular vector
+            perp = np.array([-v[1], v[0], 0]) / d
+    
+            # Tangent case
+            if h == 0:
+                return [p0]
+    
+            p1 = p0 + h * perp
+            p2 = p0 - h * perp
+    
+            return [p1, p2]
+    
+        p12 = circle_circle_intersections(centers[0],radii[0],centers[1],radii[1])
+        # checking if g is calculated correct or not
+        dot = Dot(point=g,radius=0.1,color=ORANGE)     
+        self.add(dot)
+        # checking outward arrow from each circle
+        arrows_from_centroid = VGroup()
+        for center in centers:
+            arrow = Arrow(start=g, end=center, buff=0.2, color=GREEN, tip_length=0.15)
+            arrows_from_centroid.add(arrow)
+        # debug text
+        text = Text(f"({g[0]:.2f},{g[1]:.2f},{g[2]:.2f})",font_size=20).to_corner(UR)
+        # text = Text(f"{p12[0]}",font_size=20).to_corner(UR)
+        self.play(FadeIn(text))
+        self.play(Create(circle1), Create(circle2), Create(circle3))
+        self.play(Create(arrows_from_centroid))
+        self.play(FadeIn(labels), FadeIn(area_text))
+        self.wait(1)
+    
